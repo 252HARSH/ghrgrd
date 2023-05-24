f5ab05725c6f4e4ce971949a26b99ee42940f338
252HARSH <134431986+252HARSH@users.noreply.github.com>
2023-05-25 01:02:25 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,47 @@
+name=input("Enter your name")
+print(name)
+
+
+num=input(int("Enter a number))
+print(num)
+
+num1=input(int("Enter first number))
+num2=input(int("Enter second number))
+sum=num1+num2
+print(sum)
+
+radius=input(int("Enter the radius of circle"))
+area_of_circle=(3.14*radius*radius)
+print(area_of_circle)
+
+side=input(int("Enter the side of square"))
+area_of_square=side*side
+print(area_of_square)
+
+base=input(int("Enter the base of triangle))
+height=input(int("Enter the height of triangle))
+area_of_triangle=0.5*base*height
+
+
+num1=input(int("enter first number"))
+num2=input(int("Enter second number"))
+num3=input(int("Enter third number"))
+avg=(num1+num2+num3)/3
+print(avg)
+
+principal=input(int("enter principal amount"))
+rate=input(int("enter rate of  interset"))
+time=input(int("Enter time"))
+amount=principal(1+(rate*time))
+print(amount)
+
+l=input(int("enter length number"))
+b=input(int("Enter breadth number"))
+h=input(int("Enter height number"))
+vol=(l*b*h)
+print(vol)
+
+l=input(int("enter length number"))
+b=input(int("Enter breadth number"))
+area=(l*b)
+print(area)
